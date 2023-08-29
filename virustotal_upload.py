45eab9ce788fa2e1c6d1a63ef889616263ce8750
kyanitemoongladesun <nils.gloin@proton.me>
2023-08-29 22:42:06 +0200

    add comments

--- virustotal_upload.py
+++ virustotal_upload.py
@@ -1,12 +1,17 @@
 import os
 import vt
 
+# Replace with your actual VirusTotal API key
 API_KEY = ''
+
+# Initialize the VirusTotal client
 client = vt.Client(API_KEY)
 
+# Replace 'path/to/file' with the actual path of the file you want to scan
 file = 'path/to/file'
 
+# Submit the file for scanning and wait for the scan to complete
 client.scan_file(file,wait_for_completion=True)
 
-
+# Close the VirusTotal client connection
 client.close
