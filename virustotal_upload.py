de7865e573d2435933825254555ce97ce2825f51
kyanitemoongladesun <nils.gloin@proton.me>
2023-09-07 14:27:25 +0200

    count files in dir

--- virustotal_upload.py
+++ virustotal_upload.py
@@ -7,8 +7,11 @@ API_KEY = ''
 # Initialize the VirusTotal client
 client = vt.Client(API_KEY)
 
-# Replace 'path/to/file' with the actual path of the file you want to scan
-file = 'path/to/file'
+# Replace 'path/to/folder' with the actual path of the folder you want to scan
+file = 'path/to/folder'
+
+#counts all files in Directory and Subdirectorys
+num_files = len([f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))])
 
 # Submit the file for scanning and wait for the scan to complete
 client.scan_file(file,wait_for_completion=True)
