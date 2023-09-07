d47079ea1cd6a533acb15b587f8f6ebf689d2bfd
kyanitemoongladesun <nils.gloin@proton.me>
2023-09-07 14:45:21 +0200

    skip files bigger than api limit

--- virustotal_upload.py
+++ virustotal_upload.py
@@ -13,16 +13,29 @@ file = 'path/to/folder'
 #counts all files in Directory and Subdirectorys
 num_files = len([f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))])
 
+# Define a list to store the file paths of files larger than 650MB
+large_files = []
+
 # Iterate through all files in folder
 for root,dirs ,filename in os.walk(folder_path):
     filepath = os.path.join(folder_path, filename)
     # Check if file is not a directory
     if os.path.isfile(filepath):
-        # Scan the file and wait for completion
-        with open(filepath, 'rb') as f:
-            analysis = client.scan_file(f, wait_for_completion=True)
+        if (os.path.getsize(file_path) / (1024 * 1024)) >= 650:
+            large_files.append(filepath)
+        else:
+            # Scan the file and wait for completion
+            with open(filepath, 'rb') as f:
+                analysis = client.scan_file(f, wait_for_completion=True)
 
 # Close the VirusTotal client connection
 client.close
 
+# Print the list of large files at the end
+if large_files:
+    print("The following files are larger than 650MB:")
+    for large_file in large_files:
+        print(f"- {large_file}")
+else:
+    print("No files are larger than 650MB.")
 print('All Scans Completed')
